7b064f91753d8fb3a428161208f67ef95bcebc42
iw53154 <iw53154@gmail.com>
2017-06-01 19:15:35 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'XXXXXXXXXXXXXXXXXXXXXXX';
+$access_token = 'YkkdgSiv7BLGLWQXoyNXPfkRrRAUd4DzjLy7icTetC7IaFml/EaqTnS83l0EM0OaZ/pd6bIzW4HkXIWzEBXT0wG7A7W4h2YIgu1Qi+IU1MNrxOz45cQC52QqjejK8SfQazkcN2mz2HLefiAsz1jgAQdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
