c6fc2ef7ab9980a3bba492e25802485ef07e72f4
iw53154 <iw53154@gmail.com>
2017-07-04 14:45:36 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -315,7 +315,7 @@
 	}
 	$host = "www.km1.io";
 	$port = "1883";
-	$username = "iw53154";
+	$username = "ratana53154";
 	$password = "iw49050222";
 	$topic = "/iw53154/room1";
 	$access_token = 'YkkdgSiv7BLGLWQXoyNXPfkRrRAUd4DzjLy7icTetC7IaFml/EaqTnS83l0EM0OaZ/pd6bIzW4HkXIWzEBXT0wG7A7W4h2YIgu1Qi+IU1MNrxOz45cQC52QqjejK8SfQazkcN2mz2HLefiAsz1jgAQdB04t89/1O/w1cDnyilFU=';
